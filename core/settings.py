dfc49bc393fead5cac9ee3fc31d3c170e3cb6ee0
Mans <mansur97.97@gmail.com>
2021-07-15 11:05:30 +0600

    Added django-cors-headers. Allowed react to connect to django

--- core/settings.py
+++ core/settings.py
@@ -40,9 +40,11 @@ INSTALLED_APPS = [
     'blog',
     'blog_api',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -133,3 +135,8 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny',
     ]
 }
+
+# Adding react port to allowed host
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000"
+]
