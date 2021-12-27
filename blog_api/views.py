a4f717f2c54c09b1c18938557e466143a36c168d
Mans <mansur97.97@gmail.com>
2021-12-27 15:58:21 +0600

    Created Custom Permission class to update data

--- blog_api/views.py
+++ blog_api/views.py
@@ -1,7 +1,19 @@
 from rest_framework import generics
+from rest_framework import permissions
 from blog.models import Post
 from .serializers import PostSerializer
-from rest_framework.permissions import DjangoModelPermissions
+from rest_framework.permissions import SAFE_METHODS, BasePermission, DjangoModelPermissions
+
+
+class PostUserWritePermission(BasePermission):
+    message = "Editing posts is restricted to the author only."
+
+    def has_object_permission(self, request, view, obj):
+
+        if request.method in SAFE_METHODS:
+            return True
+
+        return obj.author == request.user
 
 
 class PostList(generics.ListCreateAPIView):
@@ -11,6 +23,7 @@ class PostList(generics.ListCreateAPIView):
     pass
 
 
-class PostDetail(generics.RetrieveDestroyAPIView):
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [PostUserWritePermission]
     queryset = Post.objects.all()
     serializer_class = PostSerializer
