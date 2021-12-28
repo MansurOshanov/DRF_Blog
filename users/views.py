16a7f7ee71dc4f7493fef354623fca5242ad7556
Mans <mansur97.97@gmail.com>
2021-12-29 01:41:50 +0600

    Created blacklist token model via jwt tools and created api endpoint to blacklist these tokens

--- users/views.py
+++ users/views.py
@@ -1,8 +1,10 @@
 from rest_framework import status
+from rest_framework import permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializers import RegisterUserSerializer
 from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.tokens import RefreshToken
 
 
 class CustomUserCreate(APIView):
@@ -15,3 +17,15 @@ class CustomUserCreate(APIView):
             if newuser:
                 return Response(status=status.HTTP_201_CREATED)
         return Response(reg_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class BlacklistTokenView(APIView):
+    permissions_classes = [AllowAny]
+
+    def post(self, request):
+        try:
+            refresh_token = request.data['refresh_token']
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+        except Exception as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
